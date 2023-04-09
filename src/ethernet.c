41f5ea34d7da1ec98268095a2bd2b2c421acf0eb
ComradeSpark <67481978@qq.com>
2023-04-09 22:49:19 +0800

    lab2-completed

--- src/ethernet.c
+++ src/ethernet.c
@@ -10,18 +10,14 @@
  */
 void ethernet_in(buf_t *buf)
 {
-    //获取数据包的起始地址
-    ether_hdr_t *eth_hdr = (ether_hdr_t *)buf->data;
-
-    // 判断数据长度，如果数据长度小于以太网头部长度，则认为数据包不完整，丢弃不处理
+    // 判断数据长度，如果数据长度小于以太网头部长度，则数据包不完整，直接丢弃
     if (buf->len < sizeof(ether_hdr_t))
         return;
 
-    // 调用buf_remove_header()函数移除加以太网包头
+    ether_hdr_t *eth_hdr = (ether_hdr_t *)buf->data;
+    // 移除以太网头
     buf_remove_header(buf, sizeof(ether_hdr_t));
-
-
-    // 调用net_in()函数向上层传递数据包
+    // 向网络层传递数据包，调整大小端
     net_in(buf, swap16(eth_hdr->protocol16), eth_hdr->src);
 }
 /**
@@ -34,19 +30,18 @@ void ethernet_in(buf_t *buf)
 void ethernet_out(buf_t *buf, const uint8_t *mac, net_protocol_t protocol)
 {
     // 长度填充
-    int data_len = buf->len;
-    if(data_len < ETHERNET_MIN_TRANSPORT_UNIT) {
-        buf_add_padding(buf, ETHERNET_MIN_TRANSPORT_UNIT - data_len);
+    if(buf->len < ETHERNET_MIN_TRANSPORT_UNIT) {
+        buf_add_padding(buf, ETHERNET_MIN_TRANSPORT_UNIT - buf->len);
     }
-    //添加以太网包头
+    // 添加以太网包头
     buf_add_header(buf, sizeof(ether_hdr_t));
     ether_hdr_t *hdr = (ether_hdr_t *)buf->data;
     memcpy(hdr->dst, mac, NET_MAC_LEN);
     memcpy(hdr->src, net_if_mac, NET_MAC_LEN);
     hdr->protocol16 = swap16(protocol);
 
+    // 网卡发送
     driver_send(buf);
-
 }
 /**
  * @brief 初始化以太网协议
